78118ccdb777a3909b8a09173cf80a02c7659ec2
JunfengZhou <k9999_ah@163.com>
2019-04-24 12:10:08 +0800

    add video

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function() {
   document.getElementById('main-content').innerHTML = 'VIDEO GUIDED SOUND SOURCE SEPARATION'
   var x = document.createElement("VIDEO");
-  x.setAttribute ("width", "640");
-  x.setAttribute ("height", "480");
+  x.setAttribute ("width", "1280");
+  x.setAttribute ("height", "960");
   x.setAttribute ("controls", "controls");
   x.setAttribute ("src", "src/videoForIcira.mp4");
   document.body.appendChild(x);
